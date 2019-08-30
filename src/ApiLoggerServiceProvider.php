c643adf3db24b9b3a45aa6f5aac4d098e215fc5e
Bobby Bouwmann <bobbybouwmann@gmail.com>
2019-08-30 23:38:11 +0200

    Initial setup guzzle logger trait

---
+++ src/ApiLoggerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Spatie\ApiLogger;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiLoggerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('apilogger.php'),
+            ], 'config');
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'apilogger');
+    }
+}
